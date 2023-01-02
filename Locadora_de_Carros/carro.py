736486c379feb10741d1e2012c69f02803fbaf32
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2023-01-02 10:06:22 -0300

    Reestruturação do código

--- Locadora_de_Carros/carro.py
+++
@@ -1,8 +0,0 @@
-from locadora import *
-
-class car:
-    def __init__(self, name, mark, price):
-        self.name = name
-        self.mark = mark
-        self.price = price
-        cars.append()
