77f4c88b22cab3c5401129c6986e6670aa911719
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-01-05 23:14:46 -0300

    Tratamento de erros finalizado, código rodando sem erros

--- Jankenpon/jankenpon.py
+++ Jankenpon/jankenpon.py
@@ -17,13 +17,39 @@ class jankenpon():
     
     def compare_choices(self, user_choice):
         self.user_choice = self.choices[user_choice]
-        self.computer_choice = self.random_choice()
-        if self.user_choice == 0:
-            pass
-        if self.user_choice == 1:
-            pass
-        if self.user_choice == 2:
-            pass
+        self.computer_choice = self.choices[self.random_choice()]
+        print(f'{self.name} used - {self.user_choice}')
+        print(f'Computer used - {self.computer_choice}')
+        
+        if self.user_choice == "Stone":
+            if self.computer_choice == "Stone":
+                print('DRAW')
+            if self.computer_choice == "Paper":
+                print('YOU LOST')
+                self.computer_points =+ 1
+            if self.computer_choice == "Scissors":
+                print('YOU WON')
+                self.player_points =+ 1
+                
+        if self.user_choice == "Paper":
+            if self.computer_choice == "Stone":
+                print('YOU WON')
+                self.player_points =+ 1
+            if self.computer_choice == "Paper":
+                print('DRAW')
+            if self.computer_choice == "Scissors":
+                print('YOU LOST')
+                self.computer_points =+ 1
+            
+        if self.user_choice == "Scissors":
+            if self.computer_choice == "Stone":
+                print('YOU LOST')
+                self.computer_points =+ 1
+            if self.computer_choice == "Paper":
+                print('YOU WON')
+                self.player_points =+ 1
+            if self.computer_choice == "Scissors":
+                print('DRAW')
         
         
         
--- Jankenpon/main.py
+++ Jankenpon/main.py
@@ -9,18 +9,32 @@ def main():
     os.system('clear')
     
     game = jankenpon(name)
-    scoreboard(game.name, game.player_points, game.computer_points)
     
+    keep_playing =  True
+    while keep_playing:
+        scoreboard(game.name, game.player_points, game.computer_points)
+        try:
+            make_your_choice(game.choices)
+            user_choice = int(input())
+            
+        except Exception as erro:
+            print(erro)
+            time.sleep(2)
+            os.system('clear')
+            continue
+        try:    
+            game.compare_choices(user_choice)
+        except Exception as erro:
+            print(erro)
+            time.sleep(2)
+            os.system('clear')
+            continue
+            
+        keep_playing = int(input('''Do you want to continue playing?
+        [1] Yes
+        [0] No
+        '''))
     
-    try:
-        make_your_choice(game.choices)
-        user_choice = int(input())
-    except Exception as erro:
-        print(erro)
-        time.sleep(3)
-        os.system('clear')
-        
-    game.compare_choices(user_choice)
     
 if __name__ == "__main__":
     main()
