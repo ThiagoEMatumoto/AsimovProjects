0e3400f90e41b42dcc205de55884d52ea3af5f0c
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-01-08 19:12:10 -0300

    Moldando a classe zombie

---
+++ Simulador_do_caos/zombie.py
@@ -0,0 +1,30 @@
+
+class zombie():
+    def __init__(self, vida, velocidade, fome):
+        self.vida = vida
+        self.velocidade = velocidade
+        self.fome = fome
+
+    def morder():
+        pass
+    
+    
+    def pular():
+        pass
+    
+    
+    def correr():
+        pass
+    
+    
+    def morrer():
+        pass
+
+
+
+class super_zombie(zombie):
+    def __init__(self, vida, velocidade, fome):
+        super().__init__(vida, velocidade, fome)
+
+
+
